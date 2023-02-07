9dd0c55f75988bd7675d917d721b513c542d4d4a
Kyle Goh <KyleGoh@Kyles-MacBook-Pro-383.local>
2023-02-07 17:02:39 +0000

    Added input text box

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,3 +1,14 @@
+import {useState} from 'react'
+
 export const AddComment= () =>{
-    return(<p>adding stuff is here</p>)
+const[commentToAdd, setCommentToAdd] = useState('')
+const handleChange = (event)=>{
+    setCommentToAdd(event.target.value)
+}
+
+    return(
+    <form>
+        <input value ={commentToAdd} onChange={handleChange} arialabel="leavecomment for " type="text"/>
+    </form>
+    )
 }
