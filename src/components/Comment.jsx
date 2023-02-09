18863ebbbceea5d73ce19f18fac5bd3a3f82f26c
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-02-09 00:32:07 +0000

    temporary message appears upon deletion

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -6,15 +6,19 @@ import { useContext, useState} from "react";
 import {UserContext} from '../contexts/User'
 import { DeleteComment } from "./DeleteComment";
 import {Loading} from './Loading'
+import { Error } from "./Error";
 
 export const Comment = ({ comment, setLocalCommentCount }) => {
   const { comment_id, body, votes, created_at, author } = comment;
   const {user} = useContext(UserContext)
   const [isDeleting, setIsDeleting] = useState(false);
+  const [err, setErr] =useState(null);
+  const  [isDeleted, setIsDeleted] = useState(false)
 
   return (
+    isDeleted ? <li className = "comment-card">comment successfully deleted</li> :
     <li className="comment-card">
-      {user===author ? <DeleteComment isDeleting= {isDeleting} setLocalCommentCount = {setLocalCommentCount} setIsDeleting = {setIsDeleting} comment_id={comment_id}/> : null}
+      {user === author ? <DeleteComment setIsDeleted ={setIsDeleted}setErr={setErr} isDeleting= {isDeleting} setLocalCommentCount = {setLocalCommentCount} setIsDeleting = {setIsDeleting} comment_id={comment_id}/> : null}
       <div className="comment-avatar-container">
         <img
           src={avatar}
@@ -25,7 +29,7 @@ export const Comment = ({ comment, setLocalCommentCount }) => {
       <div className="comment-card-details">
         <h5>{author}</h5>
         <p>{body}</p>
-      {isDeleting? <Loading/> :null}
+      { err ? <Error err={err}/> : isDeleting? <Loading/> :null}
       </div>
       <p className="comment-card-footer">
         {formatDateTime(created_at)} {votes} <img alt="votes" src={upVote} />
--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,13 +1,18 @@
 import remove from "../svg/remove.svg";
 import { deleteComment } from "../utils/api-requests";
 
-export const DeleteComment = ({ comment_id, isDeleting,setIsDeleting, setLocalCommentCount}) => {
+export const DeleteComment = ({ comment_id, isDeleting, setIsDeleting, setLocalCommentCount, setErr, setIsDeleted}) => {
     const handleClick = () => {
     setIsDeleting(true);
     deleteComment(comment_id).then(()=>{
         setLocalCommentCount((currCount)=>currCount-1)
         setIsDeleting(false);
-    });
+        setIsDeleted(true);
+    })
+    .catch((err)=>{
+        setLocalCommentCount((currCount)=>currCount-1)
+        setErr("Something went wrong, please try again")
+    })
   };
 
   return (
