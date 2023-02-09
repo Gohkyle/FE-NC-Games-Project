b5393f94f4a95857053dfd58029569fd3894195b
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-02-09 20:47:14 +0000

    added isLoading to category fetch

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,29 +1,34 @@
-import { useEffect, useState } from "react"
-import { getCategories } from "../utils/api-requests"
-import {Category} from './Category'
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { getCategories } from "../utils/api-requests";
+import { Category } from "./Category";
+import { Link } from "react-router-dom";
+import { Loading } from "./Loading";
 
 export const Categories = () => {
-const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-        getCategories().then((categoriesFromApi)=>{
-            setCategories(categoriesFromApi)
-        })
-    }, [])
+  useEffect(() => {
+    getCategories().then((categoriesFromApi) => {
+      setIsLoading(false);
+      setCategories(categoriesFromApi);
+    })
+  }, []);
 
-    return (
+  return isLoading ? (
+    <Loading />
+  ) : (
     <section>
-        <ul className= "category-card-container">
-            <li className= "category-card">
-                <Link to="/reviews">
-                <h2>ALL GAMES</h2>
-                </Link>
-            </li>
-            {categories.map((category)=>{
-                return <Category key={category.slug} category={category}/>
+      <ul className="category-card-container">
+        <li className="category-card">
+          <Link to="/reviews">
+            <h2>ALL GAMES</h2>
+          </Link>
+        </li>
+        {categories.map((category) => {
+          return <Category key={category.slug} category={category} />;
         })}
-        </ul>
+      </ul>
     </section>
-    )
-}
+  );
+};
