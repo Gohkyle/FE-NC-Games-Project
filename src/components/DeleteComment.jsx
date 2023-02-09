9427dcfe24e03a623766446ea9d64b5cc589dd12
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-02-09 20:55:46 +0000

    see previous commit

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,8 +1,8 @@
 import remove from "../svg/remove.svg";
 import { deleteComment } from "../utils/api-requests";
 
-export const DeleteComment = ({ comment_id, isDeleting, setIsDeleting, setLocalCommentCount, setErr, setIsDeleted}) => {
-    const handleClick = () => {
+export const DeleteComment = ({ comment_id, isDeleting, setIsDeleting, setLocalCommentCount, setErr, setIsDeleted}) => {  
+  const handleClick = () => {
     setIsDeleting(true);
     deleteComment(comment_id).then(()=>{
         setLocalCommentCount((currCount)=>currCount-1)
