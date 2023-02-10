d68ce76288e770bb3ba3641b9194aa5c4747acd8
Kyle Goh <KyleGoh@Kyles-MacBook-Pro-383.local>
2023-02-10 12:03:06 +0000

    renamed pages for betterUI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
-import { ReviewsPage } from "./components/ReviewsPage";
-import { ReviewPage } from "./components/ReviewPage";
+import { ReviewsPage } from "./components/PageReviews";
+import { ReviewPage } from "./components/PageReview";
 import { HomePage } from "./components/HomePage";
 import { Routes, Route } from "react-router-dom";
-import { ErrorPage } from "./components/ErrorPage";
+import { ErrorPage } from "./components/PageError";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/reviews" element={<ReviewsPage />}></Route>
-        <Route path="/reviews/:review_id" element={<ReviewPage />}></Route>
-        <Route path="/category/:category" element={<ReviewsPage />}></Route>
-        <Route path="*" element={<ErrorPage />}></Route>
+        <Route path="/" element={<PageHome />}></Route>
+        <Route path="/reviews" element={<PageReviews />}></Route>
+        <Route path="/reviews/:review_id" element={<PageReview />}></Route>
+        <Route path="/reviews/add" element= {AddReviewPage/>}></Route>
+        <Route path="/category/:category" element={<PageReviews />}></Route>
+        <Route path="*" element={<PageError />}></Route>
       </Routes>
     </div>
   );
