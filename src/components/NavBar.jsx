4c29a60a1eea00011740c70d99e2b8de89dac873
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-03-06 18:52:04 +0000

    updated nav bar to include userimg

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export const NavBar=() =>{
     const {loggedInUser:{username}} = useContext(UserContext)
 
     return (<nav>
-        <Link to="/" className="Nav-item">{username? <div><p>{username}</p><UserIcon user={username}/></div> : "Login"}</Link>
+        <Link to="/" className="Nav-item">{username? <div className="nav-bar-login-container"><p>{username}</p><UserIcon user={username}/></div> : "Login"}</Link>
         <Link to="/home" className ="Nav-item">Home</Link>
         <Link to="/reviews/add" className="Nav-item">Write a Review</Link>
     </nav>)
