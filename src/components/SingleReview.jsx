b2a4da9293ed0816bc0502bd9641d3b221cdd9a3
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-03-07 21:08:47 +0000

    added div for formatting details

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -13,10 +13,10 @@ export const SingleReview = () => {
   const [review, setReview] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [isHidden, setIsHidden] = useState(true);
-  const [localCommentCount, setLocalCommentCount] =useState(0)
+  const [localCommentCount, setLocalCommentCount] = useState(0);
 
   const [err, setErr] = useState(null);
-  const [pageErr, setPageErr] = useState(null)
+  const [pageErr, setPageErr] = useState(null);
   const { review_id } = useParams();
 
   const {
@@ -32,49 +32,69 @@ export const SingleReview = () => {
 
   useEffect(() => {
     getReview(review_id)
-    .then((reviewFromApi) => {
-      setReview(reviewFromApi);
-      setLocalCommentCount(comment_count)
-      setIsLoading(false);
-    })
-    .catch(({response:{data:{msg}}})=>{
-      setPageErr(msg);
-    })
+      .then((reviewFromApi) => {
+        setReview(reviewFromApi);
+        setLocalCommentCount(comment_count);
+        setIsLoading(false);
+      })
+      .catch(
+        ({
+          response: {
+            data: { msg },
+          },
+        }) => {
+          setPageErr(msg);
+        }
+      );
   }, [review_id, comment_count]);
 
-if (pageErr) {
-  return <h2>{pageErr}</h2>
-}
+  if (pageErr) {
+    return <h2>{pageErr}</h2>;
+  }
 
   return isLoading ? (
     <Loading />
   ) : (
     <main className="single-review-container">
       <div className="single-review-header">
-      <h4 >{title}</h4>
-      <img
-        src={review_img_url}
-        alt={`BoardGame: ${title}`}
-        className="single-review-img"
-      />
-      </div >
-      <div className="single-review-details">
-        <div className="single-review-user-icon-container">
-          <UserIcon user={owner} />
+        <h4>{title}</h4>
+        <img
+          src={review_img_url}
+          alt={`BoardGame: ${title}`}
+          className="single-review-img"
+        />
+      </div>
+      <div className="single-review-details-container">
+        <div className= "single-review-details-header">
+          <div className="single-review-user-icon">
+            <UserIcon user={owner} />
           </div>
-        <h2>{owner}</h2>
-        <p>{designer}</p>
-        <p>{formatDate(created_at)}</p>
+          <div className="single-review-details">
+            <h2>{owner}</h2>
+            <p>{designer}</p>
+            <p>{formatDate(created_at)}</p>ß{" "}
+          </div>
+        </div>
         <p>{review_body}</p>
         {err ? <Error err={err} /> : null}
         <span>
-          <ReviewVoting err= {err} review_id={review_id} setErr={setErr} votes={votes} />
+          <ReviewVoting
+            err={err}
+            review_id={review_id}
+            setErr={setErr}
+            votes={votes}
+          />
           <ReviewComment
             setIsHidden={setIsHidden}
             comment_count={localCommentCount}
           />
         </span>
-        {isHidden ? null : <Comments setIsHidden={setIsHidden} setLocalCommentCount = {setLocalCommentCount}/>}
+        {isHidden ? null : (
+          <Comments
+            setIsHidden={setIsHidden}
+            setLocalCommentCount={setLocalCommentCount}
+          />
+        )}
       </div>
     </main>
   );
