97c7360547eb20bedabaef361ab5dab072bd218e
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-03-14 00:32:11 +0000

    removed usericon component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,14 @@
 import{Link} from 'react-router-dom'
 import { UserContext } from '../contexts/User'
 import { useContext } from 'react'
-import {UserIcon} from './UserIcon'
-
 
 export const NavBar=() =>{
-    const {loggedInUser:{username}} = useContext(UserContext)
+    const {loggedInUser:{username,avatar_url}} = useContext(UserContext)
 
     return (
     <nav>
         <Link to="/" className="nav-item">
-            {username? <div className="nav-login-container"><p>{username}</p><UserIcon user={username}/></div> : "Login"}
+            {username? <div className="nav-login-container"><p>{username}</p><img src={avatar_url} alt={`${username}'s avatar`} className="user-icon-img"/></div> : "Login"}
         </Link>
         <Link to="/home" className ="nav-item">Home</Link>
         <Link to="/reviews/add" className="nav-item">Write a Review</Link>
