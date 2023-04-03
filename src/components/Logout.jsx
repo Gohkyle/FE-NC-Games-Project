22bb05d82dbbb5d9729e2131344bebe2d901859d
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-04-03 11:39:44 +0100

    formatting for user-icon-image

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,7 +2,9 @@ export const Logout = ({name, username, avatar_url, setLoggedInUser}) => {
     return (
         <section className="login-form">
             <p>Welcome back {name}!</p>
-            <img src={avatar_url} alt={`${username}'s avatar`} className="user-icon-img"/>
+            <div className = "user-icon-container">
+                <img src={avatar_url} alt={`${username}'s avatar`} className="user-icon-img"/>
+            </div>
             <button className="login-form-item" onClick={()=>{ setLoggedInUser({})}}>Logout</button>
         </section>
     )
