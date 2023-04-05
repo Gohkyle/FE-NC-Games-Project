17b147392527711a2e6c28b81cf9559af9c44831
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-04-05 23:23:46 +0100

    cleaned up code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import{Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { UserContext } from '../contexts/User'
 import { useContext } from 'react'
 
@@ -8,7 +8,14 @@ export const NavBar=() =>{
     return (
     <nav>
         <Link to="/" className="nav-item">
-            {username? <div className="nav-login-container"><p>{username}</p><img src={avatar_url} alt={`${username}'s avatar`} className="user-icon-img"/></div> : "Login"}
+            {username
+                ? 
+                <div className="nav-login-container">
+                    <img src={avatar_url} alt={`${username}'s avatar`} className="user-icon-img"/>
+                    <p>{username}</p>
+                </div> 
+                : "Login"
+            }
         </Link>
         <Link to="/home" className ="nav-item">Home</Link>
         <Link to="/reviews/add" className="nav-item">Write a Review</Link>
